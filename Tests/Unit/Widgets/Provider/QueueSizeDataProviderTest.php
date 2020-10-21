c5e2d908c72bcae78931102d3d46bfdc81fa0e54
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-10-21 19:08:28 +0200

    [TASK] Add CI environment (#4)
    - Add initial phpunit.xml.dist
    - Add test for widget data provider
    - Configure Github actions

---
+++ Tests/Unit/Widgets/Provider/QueueSizeDataProviderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) 2020 FriendsOfCrawler <mail@treupo.de>
+ *
+ * This file is part of the TYPO3 Crawler Widgets Extension.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace FriendsOfCrawler\CrawlerWidget\Unit\Tests\Widgets\Provider\QueueSizeDataProvider;;
+
+use AOE\Crawler\Domain\Repository\QueueRepository;
+use FriendsOfCrawler\CrawlerWidget\Widgets\Provider\QueueSizeDataProvider;
+use PHPUnit\Framework\TestCase;
+
+class QueueSizeDataProviderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function getNumberReturnsCorrectResult(): void
+    {
+        $queueRepositoryStub = $this->createStub(QueueRepository::class);
+        $queueRepositoryStub
+            ->method('countUnprocessedItems')
+            ->willReturn(42);
+
+        $subject = new QueueSizeDataProvider($queueRepositoryStub);
+
+        self::assertSame(42, $subject->getNumber());
+    }
+}
